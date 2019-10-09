e0585c71f407308625aca36a2ef9a51381274977
alexis-houpert <houpert.alexis@gmail.com>
2019-10-09 09:20:48 +0200

    1 Alexis

---
+++ connexion_base.php
@@ -0,0 +1,39 @@
+<?php
+
+    /** Fonction permettant la connexion d'un utilisateur */
+    function connexion($mail, $mdp)
+    {
+        $dbLink = dbConnect('mysql-groupehcbc.alwaysdata.net','191114','Zhamster13');
+
+        selectDb($dbLink, 'groupehcbc_projet');
+
+        $query = $dbLink->query("SELECT mail, mdp, nom FROM user WHERE mail LIKE '$mail'");
+        if($query == FALSE) {
+            die ('Erreur SQL');
+        }
+        $posts = $query->fetch_all(PDO::FETCH_ASSOC);
+
+        $mailBD = $posts[0][0];
+        $mdpBD = $posts [0][1];
+        $nom = $posts [0][2];
+
+
+        if ($mailBD == $mail) {
+            $ver = sha1($mdp);
+            if ($ver == $mdpBD) {
+                echo 'Connexion réussite !';
+                session_start();
+                $_SESSION['nom'] = $nom;
+                $_SESSION['mdp'] = $_POST ['mdp'];
+                echo '<meta http-equiv="refresh" content="4;URL=page_membre.php" />';
+            }
+            else {
+                echo 'Identifiant ou MDP incorrect';
+                echo '<meta http-equiv="refresh" content="4;URL=index.php" />';
+            }
+        }
+        else {
+            echo 'Identifiant ou MDP incorrect';
+            echo '<meta http-equiv="refresh" content="4;URL=index.php" />';
+        }
+    }
---
+++ discussion.php
@@ -0,0 +1,76 @@
+<?php
+
+class discussion
+{
+    private $titre;
+    private $nbMessage;
+    private $dateCreation;
+    private $listeUtilisateurs;
+
+    public function __construct($titre, $nbMessage, $dateCreation, $listeUtilisateurs)
+    {
+        $this -> SetTitre($titre);
+        $this -> SetNbMessage($nbMessage);
+        $this -> SetDateCreation($dateCreation);
+        $this -> setListeUtilisateurs($listeUtilisateurs);
+    }
+
+    public function __destruct()
+    {
+        echo 'La discussion a été correctement supprimée';
+    }
+
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+    }
+
+    public function setNbMessage($nbMessage)
+    {
+        $this->nbMessage = $nbMessage;
+    }
+
+    public function setDateCreation($dateCreation)
+    {
+        $this->dateCreation = $dateCreation;
+    }
+
+    public function setListeUtilisateurs($listeUtilisateurs)
+    {
+        $this->listeUtilisateurs = $listeUtilisateurs;
+    }
+
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    public function getNbMessage()
+    {
+        return $this->nbMessage;
+    }
+
+    public function getDateCreation()
+    {
+        return $this->dateCreation;
+    }
+
+    public function getListeUtilisateurs()
+    {
+        return $this->listeUtilisateurs;
+    }
+
+    public function showDiscussion()
+    {
+        echo $this->getTitre();
+        echo '<br />';
+        echo $this->getNbMessage();
+        echo '<br />';
+        echo $this->getDateCreation();
+        echo '<br />';
+        echo $this->getListeUtilisateurs();
+        echo '<br />';
+    }
+}
+
+?>
